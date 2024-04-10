0f2e49e429d7e48416c8e88dbc622eb70a749f17
Saurav Saxena <sauravsrivastava121@gmail.com>
2024-04-10 17:32:07 +0530

    next commit

--- Instruction.py
+++ Instruction.py
@@ -4,3 +4,12 @@
 #python.exe -m pip install --upgrade pip 
 #pip install mysqlclient
 #pip install mysql-connector-python
+#for running the application flask application
+#flask --debug run
+
+#for react application 
+#after cloning the project goes root folder of react application with visual stdio integerated terminal
+#run command npm i -f
+#npm run dev
+
+#any issue facing please call me on my cell:7061036502
